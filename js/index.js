45fc109c5216fc40cd51cab12ddc9cc62137be83
Lazlo Studio <lazlo.studio@gmail.com>
2022-08-18 00:38:44 +0300

    refactored repos

--- js/index.js
+++ js/index.js
@@ -75,3 +75,59 @@ const sum = odds.reduce( (acc, el) => acc + el, 0);
 
 console.log(sum);
 
+
+
+// Code below is from js repo which was deleted
+
+const a = parseInt(prompt("Enter the number: "));
+const resultSum = fibSum(a);
+const resultArray = fibArray(a);
+const resultRecurse = fibRecurse(a);
+alert("Here is Fibonacci number by sum: " + resultSum);
+alert("Here is Fibonacci number by array: " + resultArray);
+alert("Here is Fibonacci number by recurse: " + resultRecurse);
+function fibSum (a) {
+    let fibFirst = 1;
+    let fibSecond = 1;
+    for (let i = 3; i <= a; i++){
+        let fibNext = fibFirst + fibSecond;
+        fibFirst = fibSecond;
+        fibSecond = fibNext;
+    }
+    return fibSecond;
+}
+function fibRecurse(a) {
+    if (a <= 1){
+        return a;
+    }
+    return fibRecurse(a - 1) + fibRecurse(a - 2);
+}
+function fibArray(a) {
+    let numberArray = [1 , 1];
+    for (let i = 3; i <= a; i++) {
+        let fibNext = numberArray[numberArray.length - 1] + numberArray[numberArray.length - 2];
+        numberArray.push(fibNext);
+    }
+    return numberArray[numberArray.length - 1];
+}
+
+// Code above is from js repo which was deleted
+
+
+// Code below is from js3 repo which was deleted
+
+//First Task
+function animal(obj){
+    return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`;
+  }
+  
+  //Second Task
+  function circleCircumference(circle) {
+    return Math.PI*circle.radius*2; ;
+  }
+  
+  //Third Task
+  var myObj = {};
+  MyObject.__proto__ = myObj;
+
+// Code above is from js3 repo which was deleted
